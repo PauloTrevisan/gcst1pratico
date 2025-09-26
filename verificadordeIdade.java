4b78d72ee6de6c5fab320b6d9dbf667d6e133dc5
PauloTrevisan <paulo.trevisan.2000@gmail.com>
2025-09-26 18:16:18 -0300

    Update verificadordeIdade.java
    Adição de cálculo de ano aproximado

--- verificadordeIdade.java
+++ verificadordeIdade.java
@@ -3,8 +3,12 @@ public class VerificadorDeIdade {
     public static void main(String[] args) {
         int idade = 17;
 
+        int anoAtual = 2025;
+        int anoNascimentoAproximado = anoAtual - idade;
+        
         System.out.println("A idade da pessoa é: " + idade);
-
+        System.out.println("O ano de nascimento aproximado é: " + anoNascimentoAproximado);
+        
         if (idade >= 18) {
             System.out.println("Maior de idade");
         } else {
